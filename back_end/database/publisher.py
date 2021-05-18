64b11257e3902d16fff8916baf25a8b677397d3b
Drakies <58969005+Drakies3172@users.noreply.github.com>
2021-05-18 22:46:50 +0700

    update branch

--- back_end/database/publisher.py
+++
@@ -1,6 +0,0 @@
-import paho.mqtt.publish as publish
-ADAFRUIT_IO_USERNAME = "Drakies3172"
-ADAFRUIT_IO_KEY = "aio_ntwl37DmkzVAn7QV3P70uawZMLPk"
-def publish_ada(feed_id,payload):
-    publish.single('Drakies3172/feeds/{}'.format(feed_id),hostname="io.adafruit.com",
-    auth={"username":ADAFRUIT_IO_USERNAME, "password":ADAFRUIT_IO_KEY},payload = payload)

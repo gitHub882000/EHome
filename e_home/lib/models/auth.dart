df905ab148ae6926a8a8d0e84e6813d0ea8800d3
gitHub882000 <gitHub882000>
2021-05-20 21:20:00 +0700

    Update auth.dart

--- e_home/lib/models/auth.dart
+++ e_home/lib/models/auth.dart
@@ -1,16 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Auth {
-  final _auth = FirebaseAuth.instance;
-  UserCredential userCredential;
+  final auth = FirebaseAuth.instance;
+  final firestoreInstance = FirebaseFirestore.instance;
+  User user;
+  final blankUserDoc = {
+    'name': '',
+    'phone': '',
+    'photoUrl': '',
+    'isAtHome': true,
+  };
+
+  Future<bool> isProfileBlank() {
+    firestoreInstance.collection('users').doc(user.uid).get().then((value) {
+      if (value.data()['name'] == '')
+        return true;
+      else
+        return false;
+    });
+  }
 
   Future<void> signUpWithEmailAndPassword({
     String email,
     String password,
   }) async {
     try {
-      this.userCredential = await _auth.createUserWithEmailAndPassword(
+      final userCredential = await auth.createUserWithEmailAndPassword(
           email: email, password: password);
+      user = userCredential.user;
+
+      // Initialize user's document on Firestore
+      firestoreInstance
+          .collection('users')
+          .doc(user.uid)
+          .set(this.blankUserDoc);
     } on FirebaseAuthException catch (e) {
       throw e;
     }
@@ -21,8 +45,9 @@ class Auth {
     String password,
   }) async {
     try {
-      this.userCredential = await _auth.signInWithEmailAndPassword(
+      final userCredential = await auth.signInWithEmailAndPassword(
           email: email, password: password);
+      user = userCredential.user;
     } on FirebaseAuthException catch (e) {
       throw e;
     }

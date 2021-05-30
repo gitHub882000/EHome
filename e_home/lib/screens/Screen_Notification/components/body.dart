989e0e83445b871cb628cdf31dd6ef75440ef2c7
Nam <66206021+OhaN2k@users.noreply.github.com>
2021-05-30 19:23:31 +0700

    update notification services

--- e_home/lib/screens/Screen_Notification/components/body.dart
+++ e_home/lib/screens/Screen_Notification/components/body.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:e_home/models/auth.dart';
 import 'package:e_home/screens/Screen_Notification/components/background.dart';
 import 'package:e_home/screens/Screen_Notification/components/firebaseCloudMessaging.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -10,6 +14,8 @@ class Body extends StatefulWidget {
 
 class _BodyState extends State<Body> {
   FirebaseNotification firebase = FirebaseNotification();
+  CollectionReference ref;
+  Auth _auth;
 
   handleAsyn() async {
     String token = await firebase.getToken();
@@ -19,37 +25,136 @@ class _BodyState extends State<Body> {
   List<bool> bedroomDevice = [true, false, true];
   bool _value = false;
 
+  Widget _buildList(DocumentSnapshot snapshot, String room) {
+    var s = Map<String, dynamic>.from(snapshot.get(room)).entries.toList();
+    return ListView.builder(
+      itemCount: s.length,
+      itemBuilder: (context, index) {
+        String _key = s[index].key;
+        bool _value = s[index].value;
+        return SwitchListTile(
+            title: Text(
+              _key,
+              style: TextStyle(color: Colors.white),
+            ),
+            value: _value,
+            onChanged: (bool toggle) {
+              setState(() {
+                _value = toggle;
+                if (_value) {
+                  firebase.subscribeTopic(_key);
+                } else {
+                  firebase.unsubScribeTopic(_key);
+                }
+              });
+            });
+      },
+    );
+  }
+
+  updateData(AsyncSnapshot<QuerySnapshot> snapshot, int index, String key,
+      bool value) {
+    snapshot.data.docs[index].reference.update({
+      key: value,
+    });
+  }
+
   @override
   void initState() {
     super.initState();
     firebase.initialize();
+    _auth = Provider.of<Auth>(context, listen: false);
+    ref = FirebaseFirestore.instance
+        .collection('users')
+        .doc(_auth.user.uid)
+        .collection('notification');
     handleAsyn();
   }
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-
     return Background(
-      child: Column(
-        children: <Widget>[
-          SwitchListTile(
-            title: Text("Temperature"),
-            selectedTileColor: Colors.white,
-            secondary: Icon(Icons.thermostat),
-            value: _value,
-            onChanged: (bool toggle) {
-              setState(() {
-                _value = toggle;
-                if (_value) {
-                  firebase.subscribeTopic('temperature');
-                } else {
-                  firebase.unsubScribeTopic('temperature');
-                }
-              });
+      child: StreamBuilder(
+        stream: ref.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return Text('Loading...');
+          return ListView.builder(
+            itemCount: snapshot.data.docs.length,
+            itemBuilder: (context, index) {
+              Map<String, dynamic> doc =
+                  Map.from(snapshot.data.docs[index].data());
+              return Column(
+                children: [
+                  Text(
+                    doc['name'],
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                  SwitchListTile(
+                    title: Text(
+                      'Sound',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    value: doc['sound'],
+                    onChanged: (bool toggle) {
+                      updateData(snapshot, index, 'sound', toggle);
+                      if (toggle) {
+                        firebase.subscribeTopic('sound');
+                      } else {
+                        firebase.unsubScribeTopic('sound');
+                      }
+                    },
+                  ),
+                  SwitchListTile(
+                    title: Text(
+                      'Light',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    value: doc['light'],
+                    onChanged: (bool toggle) {
+                      updateData(snapshot, index, 'light', toggle);
+                      if (toggle) {
+                        firebase.subscribeTopic('light');
+                      } else {
+                        firebase.unsubScribeTopic('light');
+                      }
+                    },
+                  ),
+                  SwitchListTile(
+                    title: Text(
+                      'Temperature',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    value: doc['temperature'],
+                    onChanged: (bool toggle) {
+                      updateData(snapshot, index, 'temperature', toggle);
+                      if (toggle) {
+                        firebase.subscribeTopic('temperature');
+                      } else {
+                        firebase.unsubScribeTopic('temperature');
+                      }
+                    },
+                  ),
+                  SwitchListTile(
+                    title: Text(
+                      'Humidity',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    value: doc['humidity'],
+                    onChanged: (bool toggle) {
+                      updateData(snapshot, index, 'humidity', toggle);
+                      if (toggle) {
+                        firebase.subscribeTopic('humidity');
+                      } else {
+                        firebase.unsubScribeTopic('humidity');
+                      }
+                    },
+                  ),
+                ],
+              );
             },
-          ),
-        ],
+          );
+        },
       ),
     );
   }

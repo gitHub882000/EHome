e21b57f1ba8fb299c8bf56d11d616a3b9778affd
gitHub882000 <gitHub882000>
2021-06-02 13:00:25 +0700

    finish dashboard

--- e_home/lib/models/sensor_history.dart
+++ e_home/lib/models/sensor_history.dart
@@ -2,21 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'dart:async';
 import 'package:e_home/utils/date_util.dart';
 
-class SensorHistoryCell {
-  DateTime date;
-  double data;
-
-  SensorHistoryCell() {
-    date = DateTime(2021);
-    data = -1;
-  }
-
-  SensorHistoryCell.withValues(String dt, double val) {
-    date = convertDateFormat(dt);
-    data = val;
-  }
-}
-
 class SensorHistory {
   /// This is for fetching history
   final _sensorReference =
@@ -24,80 +9,66 @@ class SensorHistory {
             'name',
             isNotEqualTo: 'RELAY',
           );
-  StreamController<Map<String, int>> _controller =
-      StreamController<Map<String, int>>();
+  StreamController<Map<String, List<dynamic>>> _controller =
+      StreamController<Map<String, List<dynamic>>>();
 
   // TODO: This value may be useful when seeing SensorHistory as a provider.
   Map<String, List<dynamic>> historyData = {
-    'DATE': List.generate(7, (_) => DateTime(2020)),
-    'LIGHT': List.generate(7, (_) => -1.0),
-    'TEMP': List.generate(7, (_) => -1.0),
-    'HUMID': List.generate(7, (_) => -1.0),
-    'SOUND': List.generate(7, (_) => -1.0),
+    'DATE': []..length = 7,
+    'LIGHT': []..length = 7,
+    'TEMP': []..length = 7,
+    'HUMID': []..length = 7,
+    'SOUND': []..length = 7,
   };
 
   /// Constantly listening to realtime update
   Stream listenToSensorHistory() {
-    _sensorReference.snapshots().listen((snapshot) {});
+    _sensorReference.snapshots().listen((snapshot) {
+      convertDocHistory(snapshot.docs);
+      _controller.add(historyData);
+    });
     return _controller.stream;
   }
 
-  /// Convert Document Data into Map<String, int> and save at this.realtimeData
   void convertDocHistory(
       List<QueryDocumentSnapshot<Map<String, dynamic>>> docs) {
-    int maxSize = -1;
     DateTime maxFirstDay = DateTime(2021, 1, 1);
 
-    /// List<<DateTime, double>>
-    List<SensorHistoryCell> lightHistory = [];
-    List<SensorHistoryCell> tempHistory = [];
-    List<SensorHistoryCell> humidHistory = [];
-    List<SensorHistoryCell> soundHistory = [];
-
     for (var doc in docs) {
-      List<Map<String, dynamic>> history = doc.data()['history'];
-      DateTime firstDay = convertDateFormat(history[0]['date']);
-      maxSize = history.length > maxSize ? history.length : maxSize;
+      DateTime firstDay = convertDateFormat(doc.data()['history'][0]['date']);
       maxFirstDay = firstDay.isAfter(maxFirstDay) ? firstDay : maxFirstDay;
     }
 
     /// Trace every doc, pass data into 4 lists
     for (var doc in docs) {
-      List<Map<String, dynamic>> history = doc.data()['history'];
-      if (convertDateFormat(history[0]['date']).isBefore(maxFirstDay)) {
-        if (doc.data()['name'] == 'TEMP-HUMID') {
-          historyData['TEMP'] = List.generate(7, (_) => -1.0);
-          historyData['HUMID'] = List.generate(7, (_) => -1.0);
-        } else {
-          historyData[doc.data()['name']] = List.generate(7, (_) => -1.0);
-        }
-        break;
+      if (doc.data()['name'] == 'TEMP-HUMID') {
+        historyData['TEMP'] = List.generate(7, (_) => -1.0);
+        historyData['HUMID'] = List.generate(7, (_) => -1.0);
+      } else {
+        historyData[doc.data()['name']] = List.generate(7, (_) => -1.0);
       }
-      else {
+      List<Map<String, dynamic>> history = (doc.data()['history'] as List)
+          .map((element) => {
+        'date': element['date'],
+        'data': element['data'],
+      })
+          .toList();
+      if (!convertDateFormat(history[0]['date']).isBefore(maxFirstDay)) {
         if (doc.data()['name'] == 'TEMP-HUMID') {
-
-        }
-      }
-      for (var cell in history) {
-        if (doc.data()['name'] == 'TEMP-HUMID') {
-          List<String> splits = cell['data'].split('-');
-          tempHistory.add(SensorHistoryCell.withValues(
-              cell['date'], double.parse(splits[0])));
-          humidHistory.add(SensorHistoryCell.withValues(
-              cell['date'], double.parse(splits[1])));
-        } else if (doc.data()['name'] == 'LIGHT') {
           for (int i = 0; i < history.length; i++) {
-            lightHistory.add(
-                SensorHistoryCell.withValues(cell['date'], history[i]['data']));
+            List<String> splits = history[i]['data'].split('-');
+            historyData['TEMP'][i] = double.parse(splits[0]);
+            historyData['HUMID'][i] = double.parse(splits[1]);
           }
         } else {
           for (int i = 0; i < history.length; i++) {
-            soundHistory.add(
-                SensorHistoryCell.withValues(cell['date'], history[i]['data']));
+            historyData[doc.data()['name']][i] = history[i]['data'];
           }
         }
       }
     }
+    historyData['DATE'] =
+        List.generate(7, (index) => maxFirstDay.add(Duration(days: index)));
   }
 
   void dispose() {
--- e_home/lib/screens/Screen_Statistics/components/body.dart
+++ e_home/lib/screens/Screen_Statistics/components/body.dart
@@ -2,17 +2,182 @@ import 'package:flutter/material.dart';
 import 'package:e_home/screens/shared_components/icon_coin.dart';
 import 'package:e_home/screens/shared_components/text_with_pre_icon.dart';
 import 'package:e_home/icons/water_drop_icons.dart';
+import 'package:intl/intl.dart';
 import 'background.dart';
-import 'package:e_home/models/realtime_sensors.dart';
+import 'package:e_home/models/sensor_history.dart';
 
 // ignore: must_be_immutable
 class Body extends StatelessWidget {
-  RealtimeSensors _realtimeSensors = RealtimeSensors();
+  SensorHistory _sensorHistory = SensorHistory();
 
   @override
   Widget build(BuildContext context) {
     // This size provides us total height and width of our screen
     Size size = MediaQuery.of(context).size;
+
+    // These are constant widgets that will not change
+    List<DataColumn> tableAttributes = [
+      DataColumn(
+        label: Text(
+          'Date',
+          style: Theme.of(context).textTheme.headline6.copyWith(
+                fontWeight: FontWeight.w600,
+                fontSize: size.height * 0.02,
+              ),
+        ),
+      ),
+      DataColumn(
+        label: Row(
+          children: [
+            IconCoin(
+              borderRadius: 5,
+              padding: EdgeInsets.all(3.0),
+              height: size.width * 0.06,
+              width: size.width * 0.06,
+              backgroundColor: Theme.of(context).accentColor.withOpacity(0.1),
+              child: Icon(
+                Icons.lightbulb,
+                color: Theme.of(context).accentColor,
+                size: size.width * 0.04,
+              ),
+            ),
+            Text(
+              '(lx)',
+              style: Theme.of(context).textTheme.bodyText1.copyWith(
+                  fontWeight: FontWeight.w600, fontSize: size.height * 0.016),
+            ),
+          ],
+        ),
+      ),
+      DataColumn(
+        label: Row(
+          children: [
+            IconCoin(
+              borderRadius: 5,
+              padding: EdgeInsets.all(3.0),
+              height: size.width * 0.06,
+              width: size.width * 0.06,
+              backgroundColor: Theme.of(context).accentColor.withOpacity(0.1),
+              child: Icon(
+                Icons.thermostat_outlined,
+                color: Theme.of(context).accentColor,
+                size: size.width * 0.04,
+              ),
+            ),
+            Text(
+              '(\u2103)',
+              style: Theme.of(context).textTheme.bodyText1.copyWith(
+                  fontWeight: FontWeight.w600, fontSize: size.height * 0.016),
+            ),
+          ],
+        ),
+      ),
+      DataColumn(
+        label: Row(
+          children: [
+            IconCoin(
+              borderRadius: 5,
+              padding: EdgeInsets.all(3.0),
+              height: size.width * 0.06,
+              width: size.width * 0.06,
+              backgroundColor: Theme.of(context).accentColor.withOpacity(0.1),
+              child: Icon(
+                Water_drop.water_drop_black_24dp,
+                color: Theme.of(context).accentColor,
+                size: size.width * 0.04,
+              ),
+            ),
+            Text(
+              '(%)',
+              style: Theme.of(context).textTheme.bodyText1.copyWith(
+                  fontWeight: FontWeight.w600, fontSize: size.height * 0.016),
+            ),
+          ],
+        ),
+      ),
+      DataColumn(
+        label: Row(
+          children: [
+            IconCoin(
+              borderRadius: 5,
+              padding: EdgeInsets.all(3.0),
+              height: size.width * 0.06,
+              width: size.width * 0.06,
+              backgroundColor: Theme.of(context).accentColor.withOpacity(0.1),
+              child: Icon(
+                Icons.surround_sound,
+                color: Theme.of(context).accentColor,
+                size: size.width * 0.04,
+              ),
+            ),
+            Text(
+              '(dB)',
+              style: Theme.of(context).textTheme.bodyText1.copyWith(
+                  fontWeight: FontWeight.w600, fontSize: size.height * 0.016),
+            ),
+          ],
+        ),
+      ),
+    ];
+
+    List<DataRow> loadingRow = [
+      DataRow(
+        cells: [
+          DataCell(
+            Text(
+              'Pending',
+              style: TextStyle(
+                color: Theme.of(context).cardColor,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          DataCell(
+            Text(
+              'Pending',
+              style: TextStyle(
+                color: Theme.of(context).cardColor,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          DataCell(
+            Text(
+              'Pending',
+              style: TextStyle(
+                color: Theme.of(context).cardColor,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          DataCell(
+            Text(
+              'Pending',
+              style: TextStyle(
+                color: Theme.of(context).cardColor,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+          DataCell(
+            Text(
+              'Pending',
+              style: TextStyle(
+                color: Theme.of(context).cardColor,
+                fontWeight: FontWeight.w600,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+          ),
+        ],
+      ),
+    ];
+
+    /// Build widget
     return Background(
       child: Column(
         children: [
@@ -54,157 +219,92 @@ class Body extends StatelessWidget {
               child: SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: StreamBuilder(
-                  stream: _realtimeSensors.listenToSensors(),
-                  builder: (context, sensors) => DataTable(
-                    columns: [
-                      DataColumn(
-                        label: Text(
-                          'Date',
-                          style: Theme.of(context).textTheme.headline6.copyWith(
-                                fontWeight: FontWeight.w600,
-                                fontSize: size.height * 0.02,
-                              ),
-                        ),
-                      ),
-                      DataColumn(
-                        label: Row(
-                          children: [
-                            IconCoin(
-                              borderRadius: 5,
-                              padding: EdgeInsets.all(3.0),
-                              height: size.width * 0.06,
-                              width: size.width * 0.06,
-                              backgroundColor: Theme.of(context)
-                                  .accentColor
-                                  .withOpacity(0.1),
-                              child: Icon(
-                                Icons.lightbulb,
-                                color: Theme.of(context).accentColor,
-                                size: size.width * 0.04,
-                              ),
-                            ),
-                            Text(
-                              '(lx)',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  .copyWith(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: size.height * 0.016),
-                            ),
-                          ],
-                        ),
-                      ),
-                      DataColumn(
-                        label: Row(
-                          children: [
-                            IconCoin(
-                              borderRadius: 5,
-                              padding: EdgeInsets.all(3.0),
-                              height: size.width * 0.06,
-                              width: size.width * 0.06,
-                              backgroundColor: Theme.of(context)
-                                  .accentColor
-                                  .withOpacity(0.1),
-                              child: Icon(
-                                Icons.thermostat_outlined,
-                                color: Theme.of(context).accentColor,
-                                size: size.width * 0.04,
-                              ),
-                            ),
-                            Text(
-                              '(\u2103)',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  .copyWith(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: size.height * 0.016),
-                            ),
-                          ],
-                        ),
-                      ),
-                      DataColumn(
-                        label: Row(
-                          children: [
-                            IconCoin(
-                              borderRadius: 5,
-                              padding: EdgeInsets.all(3.0),
-                              height: size.width * 0.06,
-                              width: size.width * 0.06,
-                              backgroundColor: Theme.of(context)
-                                  .accentColor
-                                  .withOpacity(0.1),
-                              child: Icon(
-                                Water_drop.water_drop_black_24dp,
-                                color: Theme.of(context).accentColor,
-                                size: size.width * 0.04,
+                  stream: _sensorHistory.listenToSensorHistory(),
+                  builder: (context, historyMap) {
+                    return DataTable(
+                      columns: tableAttributes,
+                      rows: historyMap.connectionState ==
+                              ConnectionState.waiting
+                          ? loadingRow
+                          : List.generate(
+                              7,
+                              (index) => DataRow(
+                                cells: [
+                                  DataCell(
+                                    Text(
+                                      DateFormat('MM-dd-yyyy').format(
+                                          historyMap.data['DATE'][index]),
+                                    ),
+                                  ),
+                                  DataCell(
+                                    historyMap.data['LIGHT'][index] > 0
+                                        ? Text(
+                                            historyMap.data['LIGHT'][index]
+                                                .toStringAsFixed(2),
+                                          )
+                                        : Text(
+                                            'Pending',
+                                            style: TextStyle(
+                                              color:
+                                                  Theme.of(context).cardColor,
+                                              fontWeight: FontWeight.w600,
+                                              fontStyle: FontStyle.italic,
+                                            ),
+                                          ),
+                                  ),
+                                  DataCell(
+                                    historyMap.data['TEMP'][index] > 0
+                                        ? Text(
+                                            historyMap.data['TEMP'][index]
+                                                .toStringAsFixed(2),
+                                          )
+                                        : Text(
+                                            'Pending',
+                                            style: TextStyle(
+                                              color:
+                                                  Theme.of(context).cardColor,
+                                              fontWeight: FontWeight.w600,
+                                              fontStyle: FontStyle.italic,
+                                            ),
+                                          ),
+                                  ),
+                                  DataCell(
+                                    historyMap.data['HUMID'][index] > 0
+                                        ? Text(
+                                            historyMap.data['HUMID'][index]
+                                                .toStringAsFixed(2),
+                                          )
+                                        : Text(
+                                            'Pending',
+                                            style: TextStyle(
+                                              color:
+                                                  Theme.of(context).cardColor,
+                                              fontWeight: FontWeight.w600,
+                                              fontStyle: FontStyle.italic,
+                                            ),
+                                          ),
+                                  ),
+                                  DataCell(
+                                    historyMap.data['SOUND'][index] > 0
+                                        ? Text(
+                                            historyMap.data['SOUND'][index]
+                                                .toStringAsFixed(2),
+                                          )
+                                        : Text(
+                                            'Pending',
+                                            style: TextStyle(
+                                              color:
+                                                  Theme.of(context).cardColor,
+                                              fontWeight: FontWeight.w600,
+                                              fontStyle: FontStyle.italic,
+                                            ),
+                                          ),
+                                  ),
+                                ],
                               ),
                             ),
-                            Text(
-                              '(%)',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  .copyWith(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: size.height * 0.016),
-                            ),
-                          ],
-                        ),
-                      ),
-                      DataColumn(
-                        label: Row(
-                          children: [
-                            IconCoin(
-                              borderRadius: 5,
-                              padding: EdgeInsets.all(3.0),
-                              height: size.width * 0.06,
-                              width: size.width * 0.06,
-                              backgroundColor: Theme.of(context)
-                                  .accentColor
-                                  .withOpacity(0.1),
-                              child: Icon(
-                                Icons.surround_sound,
-                                color: Theme.of(context).accentColor,
-                                size: size.width * 0.04,
-                              ),
-                            ),
-                            Text(
-                              '(dB)',
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .bodyText1
-                                  .copyWith(
-                                      fontWeight: FontWeight.w600,
-                                      fontSize: size.height * 0.016),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
-                    rows: [
-                      DataRow(
-                        cells: [
-                          DataCell(
-                            Text('${sensors.data['LIGHT']}'),
-                          ),
-                          DataCell(
-                            Text('${sensors.data['LIGHT']}'),
-                          ),
-                          DataCell(
-                            Text('${sensors.data['TEMP']}'),
-                          ),
-                          DataCell(
-                            Text('${sensors.data['HUMID']}'),
-                          ),
-                          DataCell(
-                            Text('${sensors.data['SOUND']}'),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                    );
+                  },
                 ),
               ),
             ),

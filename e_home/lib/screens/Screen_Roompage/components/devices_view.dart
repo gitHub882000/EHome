af83def3372f88b3e360aaec86a28e1dc8a362cd
Drakies <58969005+Drakies3172@users.noreply.github.com>
2021-06-03 09:59:52 +0700

    modify real server url

--- e_home/lib/screens/Screen_Roompage/components/devices_view.dart
+++ e_home/lib/screens/Screen_Roompage/components/devices_view.dart
@@ -154,7 +154,7 @@ Future<DeviceModel> sendData(String account, String data, String name,
     String topic, String update) async {
   // var url = Uri.parse('localhost:5000/publisher/khang');
   // var response = await http.post(url, body: {'name': 'doodle', 'color': 'blue'});
-  String apiUrl = "http://10.0.2.2:5000/publisher/Tam";
+  String apiUrl = "https://ehomee.azurewebsites.net/publisher/Tam";
 
   final json = {
     'account': account,
